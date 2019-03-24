eff1b70477ab1eb189232d4b1f348844ffeef45a
moroz <wertixvost@gmail.com>
2019-03-24 20:20:14 +0200

    Production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: {
         index: './app.js',
     },
-    devtool: 'inline-source-map',
+    //devtool: 'inline-source-map',
     output: {
         filename: 'app.bundle.js',
         path: path.resolve(__dirname, 'dist')
